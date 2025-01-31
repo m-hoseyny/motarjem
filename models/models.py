648429500fb21c533052d76e09b0cfb35e92cf09
Mohammad Hosseini <mohammad.hoseyny@gmail.com>
2025-01-31 19:26:57 +0330

    feat: alter user id

--- models/models.py
+++ models/models.py
@@ -88,7 +88,7 @@ class BotUser(Base):
     __tablename__ = "bot_users"
 
     id = Column(Integer, primary_key=True, index=True)
-    telegram_id = Column(Integer, unique=True, index=True)
+    telegram_id = Column(BigInteger, unique=True, index=True)
     username = Column(String, unique=True, index=True)
     first_name = Column(String)
     last_name = Column(String, nullable=True)
